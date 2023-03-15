bae392d19d66c5ef8e646e6614e705d534aa3182
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-03-15 19:30:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  if (arr[mid] == value) {
+    for (int i = mid; i < size && arr[i] == value; ++i)
+      count++;
+    break;
+  }
 }
