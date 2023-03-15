b9d2ca19b328a70c06bd5e109ef94b8e6777a92d
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-03-15 21:21:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  for (int i = 0; i < size; i++)
+  int count = 0;
+  for (int i = 0; i < size; ++i)
     if (arr[i] == value) {
       count+=1;
-    }
+  }
+  if (count > 0)
+    return count;
+  else
     return 0;
 }   
